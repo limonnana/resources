8b0400beaa1cdc1b901b45e0752dd8cbb6cfc257
eyal <rosenzvaig@gmail.com>
2015-09-02 12:14:54 +0300

    commit septiembre 2

--- resources/WebContent/hola.js
+++ resources/WebContent/hola.js
@@ -1,16 +1,24 @@
+
 $(document).ready(function() {
     $.ajax(
     {
-      url: "http://localhost:8080/resources/webservice.html",
-      dataType: 'json'
+       type: "GET", 
+    	url: "http://localhost:8080/resources/webservice.html",
+       /*headers: {
+           Accept:"application/json",
+           "Access-Control-Allow-Origin": "http://localhost:8080*"
+       },*/
+       dataType: "json",
+      // url: "http://rest-service.guides.spring.io/greeting",
+      
     })
 .done (function(data) { $('.greeting-id').append(data.id); $('.greeting-content').append(data.content); })                                              //alert("Success: " + data.id + " " + data.content) ; })
 .fail   (function()     { alert("Error")   ; })
 });
-    
-    
-    
-    
+
+
+
+
     /*.then(function(data) {
        $('.greeting-id').append(data.id);
        $('.greeting-content').append(data.content);
